b062644f1a38ecb36438da5d82cde14adbe92ffd
Darpan <madiadarpan@gmail.com>
2021-03-31 00:29:16 +0530

    Initial commit

---
+++ cqp-angular8-springboot-client/src/app/transfer-part-list/historical/tpl-historical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historical',
+  templateUrl: './tpl-historical.component.html',
+  styleUrls: ['./tpl-historical.component.css']
+})
+export class TplHistoricalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
